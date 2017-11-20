094e65d81caf6e5e1446ba8e184007bfb3c98c41
Ali Yousefi <aliyousefi.tec@gmail.com>
2017-11-20 12:12:14 +0330

    fix  curl_init()

--- getUpdate.php
+++ getUpdate.php
@@ -5,40 +5,35 @@ $url = "http://127.0.0.1/bot/webhook";
 
 
 $apiUrl = "https://api.telegram.org/bot".$botToken;
-try{
 
-$update = file_get_contents($apiUrl."/getupdates");
+try {
+    $update = file_get_contents($apiUrl."/getupdates");
 
-if($update == false)
-{
-	throw new Exception("Check your internet Conncetion", 1);
-}
+    if ($update == false) {
+        throw new Exception("Check your internet Conncetion", 1);
+    }
 
-$u = json_decode($update, true);
+    $u = json_decode($update, true);
 
-if($u['ok']){
-    echo "Count : " . count($u['result']); 
-    $update = $u['result'];
-    $count = count($update);
-    if ($count > 0) {
-    for ($i = 0; $i < $count; $i++) {
-            sendToTelegram($update[$i], $url);
+    if ($u['ok']) {
+        echo "Count : " . count($u['result']);
+        $update = $u['result'];
+        $count = count($update);
+        if ($count > 0) {
+            for ($i = 0; $i < $count; $i++) {
+                sendToTelegram($update[$i], $url);
+            }
+            $update = file_get_contents($apiUrl."/getupdates?offset=".($update[$i-1]['update_id']+1));
         }
-        $update = file_get_contents($apiUrl."/getupdates?offset=".($update[$i-1]['update_id']+1));
-    } 
-}
-
-} catch(Exception $e)
-{
+    }
+} catch (Exception $e) {
     echo $e->getMessage() . "\n";
     echo "Check your url \n";
     exit;
 }
-function sendToTelegram($data, $url) {
 
-
-$curl = curl_init();
-
+function sendToTelegram($data, $url)
+{
     $data_string = json_encode($data);
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
@@ -52,5 +47,4 @@ $curl = curl_init();
     ]);
 
     $result = curl_exec($curl);
-
 }
